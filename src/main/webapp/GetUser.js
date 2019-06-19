d86bf2956cb95aab2305880940efa36340d39f23
Danny <Danny>
2019-06-19 17:33:02 +0100

    edited the javaScript file for GetUser

--- src/main/webapp/GetUser.js
+++ src/main/webapp/GetUser.js
@@ -28,6 +28,7 @@ function getUser() {
     let input1 = document.getElementById("input1").value;
     let input2 = document.getElementById("input2").value;
     let inputNames = input1 + input2;
+    const urlByUser = `http://35.242.137.2:8080/workoutTracker-1.0/api/users/username/${inputNames}`;
     requestData(urlByUser, "GET", "").then((x) =>{
         let data = JSON.parse(x.responseText);
         console.log(data);
