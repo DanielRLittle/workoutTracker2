74ae2ffd4f3cd6632c69946714849d7635ec0063
Daniel Little <daniel.r.little.1994@gmail.com>
2019-06-24 16:15:58 +0100

    editing oneToMany

--- src/main/java/com/qa/rest/WorkoutEndPoints.java
+++ src/main/java/com/qa/rest/WorkoutEndPoints.java
@@ -7,9 +7,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.qa.model.User;
 import com.qa.model.Workout;
+import com.qa.repository.UserRepo;
 import com.qa.repository.WorkoutRepo;
 
 @Path("/")
@@ -18,12 +21,18 @@ public class WorkoutEndPoints {
 	@Inject
 	WorkoutRepo wr;
 	
+	@Inject
+	UserRepo ur;
+	
 	@PUT
 	@Consumes({"application/json"})
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/workout/{user_id}")
-	public User addingWorkout(Workout workout, @PathParam("user_id") int id) {
+	public Response addingWorkout(Workout workout, @PathParam("user_id") int id) {
+		if (ur.readUser(id).equals(null)) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
 		User user = wr.addWorkout(workout, id);
-		return user;
+		return Response.ok(user).build();
 	}
 }
