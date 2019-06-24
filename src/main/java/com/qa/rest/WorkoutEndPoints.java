914eb2805beb7edb862253910ef8a586302f3ee6
Danny <Danny>
2019-06-24 09:19:56 +0100

    nothing changed

--- src/main/java/com/qa/rest/WorkoutEndPoints.java
+++ src/main/java/com/qa/rest/WorkoutEndPoints.java
@@ -2,7 +2,6 @@ package com.qa.rest;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
